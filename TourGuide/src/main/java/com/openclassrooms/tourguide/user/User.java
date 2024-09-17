0a22752d40d1fdf3a64e40b1fec35bc18127268a
yaser <y.r.k.r.2011@gmail.com>
2024-09-17 23:00:18 +0200

    first test stuck

--- TourGuide/src/main/java/com/openclassrooms/tourguide/user/User.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/user/User.java
@@ -73,6 +73,11 @@ public class User {
 		if(userRewards.stream().filter(r -> !r.attraction.attractionName.equals(userReward.attraction)).count() == 0) {
 			userRewards.add(userReward);
 		}
+
+
+//		userRewards.stream().dropWhile(userReward1 -> userReward1.attraction.attractionName.equals(userReward1.attraction))
+//				.forEach(Reward->userRewards.add(Reward));
+
 	}
 	
 	public List<UserReward> getUserRewards() {
